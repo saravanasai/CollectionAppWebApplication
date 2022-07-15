b441320fef05f770734adb5aa866f45f4679341c
Saravana Sai <saravanasai002@gmail.com>
2022-07-15 19:36:04 +0530

    finaly fixed complement checkbox added condition & added fixed total balance bug on update plan

--- resources/js/config.js
+++ resources/js/config.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const token = localStorage.getItem('auth_token')
  const api = axios.create({
-    // baseURL: 'http://127.0.0.1:8000/api/v1/',
-    baseURL: 'http://rajametals.co.in/api/v1/',
+    baseURL: 'http://127.0.0.1:8000/api/v1/',
+    // baseURL: 'http://rajametals.co.in/api/v1/',
     timeout: 10000,
     headers: {
         'Accept': 'application/json',
